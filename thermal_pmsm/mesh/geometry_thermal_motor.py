8a98a9194c7a424f235acd06564c7f59232afbda
Vertti Aalto <vertti.aalto@km.kongsberg.com>
2026-08-08 01:39:38 +0300

    Good results and seems to work

--- thermal_pmsm/mesh/geometry_thermal_motor.py
+++ thermal_pmsm/mesh/geometry_thermal_motor.py
@@ -187,7 +187,8 @@ gmsh.model.mesh.generate(2)
 
 from pathlib import Path
 # write the .msh to same folder
-mesh_file = Path(__file__).with_name("mesh_pmsm_2d.msh")
+mesh_file = Path(__file__).with_name("mesh_pmsm_2d.bdf")
+
 
 gmsh.write(str(mesh_file))
 
